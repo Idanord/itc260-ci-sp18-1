4bff99ed5c27bc722270e54d69645896414e7da4
Idanord <33676867+Idanord@users.noreply.github.com>
2018-04-23 21:23:44 -0700

    Loads initial news application
    initial news application

---
+++ ci-sp18/application/views/news/success.php
@@ -0,0 +1,13 @@
+<?php
+//application/views/news/success.php
+
+$this->load->view($this->config->item('theme') . 'header');
+?>
+
+<h1>Yay!</h1>
+<p>In theory your data was entered!</p>
+
+<?php
+
+$this->load->view($this->config->item('theme') . 'footer');
+?>
